de74fbdb0b17f08933e49e639e956874fec3602b
Gary Johnson <gary@gjtt.com>
2015-06-13 12:20:16 -0400

    Testing travis commit

--- shovel.py
+++ shovel.py
@@ -3,7 +3,7 @@ import subprocess
 
 @shovel.task
 def test():
-    subprocess.call('behave --tags ~@wip --tags ~@later', shell=True)
+    subprocess.call('KVAUT_LOG=DEBUG behave --tags ~@wip --tags ~@later --no-capture --no-logcapture --logging-level DEBUG', shell=True)
 
 @shovel.task
 def wip():
