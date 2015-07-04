9a3f4247e038d7aee379f9099d371bd60af100fb
Gary Johnson <gary@gjtt.com>
2015-07-04 12:37:42 -0400

    Only run behave on travis

--- shovel.py
+++ shovel.py
@@ -6,6 +6,10 @@ def test():
     subprocess.call('tox', shell=True)
 
 @shovel.task
+def ci_test():
+    subprocess.call('behave --tags ~@wip --tags ~@later', shell=True)
+
+@shovel.task
 def debug_test():
     subprocess.call('KVAUT_LOG=DEBUG behave --tags ~@wip --tags ~@later --no-capture --no-logcapture --logging-level DEBUG', shell=True)
 
