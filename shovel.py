1506bcdee2519b2ca11155dc83f4036efd622fea
Gary Johnson <gary@gjtt.com>
2015-07-04 12:27:08 -0400

    Testing travis build again

--- shovel.py
+++ shovel.py
@@ -3,6 +3,10 @@ import subprocess
 
 @shovel.task
 def test():
+    subprocess.call('tox', shell=True)
+
+@shovel.task
+def debug_test():
     subprocess.call('KVAUT_LOG=DEBUG behave --tags ~@wip --tags ~@later --no-capture --no-logcapture --logging-level DEBUG', shell=True)
 
 @shovel.task
