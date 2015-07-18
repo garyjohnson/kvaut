b44901409e297cf6a48212e31de7ef34e37ec306
Gary Johnson <gary@gjtt.com>
2015-07-17 22:09:45 -0400

    v0.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='kvaut',
-    version='0.0.5',
+    version='0.0.6',
     author='Gary Johnson',
     author_email = 'gary@gjtt.com',
     description = 'Automation for BDD testing Kivy apps',
